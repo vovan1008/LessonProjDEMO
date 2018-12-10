3399982bb000e71ccbc27bb74c38afa77f3b5171
DHaidenko <dgaidenko@gmail.com>
2018-12-10 19:49:46 +0200

    Initial commit

---
+++ src/Phone.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Student on 10.12.2018.
+ */
+public class Phone {
+    String model, number;
+    double weight;
+
+}
+
+
---
+++ src/PhoneDemo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Student on 10.12.2018.
+ */
+public class PhoneDemo {
+    public static void main(String[] args) {
+        Phone phone1= new Phone();
+        Phone phone2= new Phone();
+        Phone phone3= new Phone();
+
+        phone1.number = "+380990000762";
+        phone1.model = "Xiaomi";
+        phone1.weight = 300;
+
+        System.out.println(phone1.number +" "+ phone1.model+ " " + phone1.weight);
+    }
+}
